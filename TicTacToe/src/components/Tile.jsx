5bff78271977e82f32b258a17bb407a55dfb3b44
unknown <omerstr1@gmail.com>
2023-06-06 11:54:23 +0300

    m

--- TicTacToe/src/components/Tile.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Tile(props) {
-  return (
-    <div className="tile" onClick={props.handleClick}>
-        <button className="tile-text">{props.value}</button>
-    </div>
-  );
-}
--- TicTacToe/src/components/WinningAnimations.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function WinningAnimation(props) {
-return (
-    <div className="WinningGif">
-        <img src={props.gif}/>
-    </div>
-)
-}
